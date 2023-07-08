86be3f9c433ecbf291d903a526cc8ae5b34c40cf
Joseph Petersen <joseph.j.petersen@gmail.com>
2023-07-08 16:28:23 -0400

    Add my solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  let joinedString = '';
 
+  if (num < 0) return 'ERROR';
+
+  for (let i = 0; i < num; i++) {
+    joinedString += string;
+  }
+  return joinedString;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+  
+  if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+  if (x < 0 || y < 0) return "ERROR";
 
+  let min = 0;
+  let max = 0;
+  let total = 0;
+
+  if (x < y){
+    min = x;
+    max = y;
+  } else {
+    min = y;
+    max = x;
+  };
+
+  for (let i = min; i <= max; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
